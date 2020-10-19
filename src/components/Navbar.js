9def74042246705b9a71868a29fbf691500ed07c
Jung <junghong91@naver.com>
2020-10-19 15:45:11 +0900

    Adding React Icons to the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import { MdFingerprint } from "react-icons/md"
+import { FaBars, FaTimes } from "react-icons/fa"
 
 function Navbar() {
+    const [click, setClick] = useState(false)
+    
+    const handleClick = () => setClick(!click)
+
     return (
         <>
             <div className="navbar">
-            
+                <div className="navbar-container container">
+                    <Link to="/" className="navbar-logo">
+                        <MdFingerprint className="navbar-icon" />
+                        Portfolio
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars />}
+                    </div>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/about" className="nav-links">
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/projects" className="nav-links">
+                                Projects
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contact" className="nav-links">
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>  
         </>
     )
