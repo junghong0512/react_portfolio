26a88674011c813fd6032dac0f2c3c643e54af97
Jung <junghong91@naver.com>
2020-10-21 01:15:50 +0900

    Navbar icon is updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import "./Button.css";
 
 const STYLES = ["btn--primary", "btn--outline"];
 
-const SIZE = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
+const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
 
 const COLOR = ["primary", "blue", "red", "green"];
 
@@ -19,7 +19,7 @@ export const Button = ({
     ? buttonStyle
     : STYLES[0];
 
-  const checkButtonSize = SIZE.includes(buttonSize) ? buttonSize : SIZE[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
 
