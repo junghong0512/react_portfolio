231f13c7a937f3ea66fab55ca5608fb51d2be476
Jung <junghong91@naver.com>
2020-10-28 13:51:16 +0900

    Creating Home Page

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Exclusive Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees",
+  buttonLabel: "Check My Work",
+  imgStart: "",
+  img: "images/home.svg",
+  alt: "Porfolio Image",
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Exclusive Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees",
+  buttonLabel: "Check My Work",
+  imgStart: "",
+  img: "images/home.svg",
+  alt: "Porfolio Image",
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Exclusive Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees",
+  buttonLabel: "Check My Work",
+  imgStart: "",
+  img: "images/home.svg",
+  alt: "Porfolio Image",
+};
+
+export const homeObjFour = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Exclusive Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees",
+  buttonLabel: "Check My Work",
+  imgStart: "",
+  img: "images/home.svg",
+  alt: "Porfolio Image",
+};
